0af7ba9cdbcf9824f62824c85eead0bdc471952e
Vala Khosravi <v.khosravi@emofid.com>
2023-08-14 14:48:17 +0330

    feat: add superuser on docker compose up

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-
-
-
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
@@ -15,5 +12,13 @@ fi
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+if [ "$DJANGO_SUPERUSER_USERNAME" ]
+then
+    python manage.py createsuperuser \
+        --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL
+fi
 
 exec "$@"
