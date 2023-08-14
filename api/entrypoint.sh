666c980837327aed3810a6264fca7627da4e9a49
Vala Khosravi <v.khosravi@emofid.com>
2023-08-14 10:29:16 +0330

    fix: entrypoint.sh not found

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -3,15 +3,13 @@
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
-
     while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
+      sleep 1
     done
 
     echo "PostgreSQL started"
 fi
 
-python manage.py flush --no-input
 python manage.py migrate
 
 exec "$@"
